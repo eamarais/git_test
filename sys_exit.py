8bd0338411183234f01637b75a0b6b2e82fb503e
Eloise Marais <55648553+eamarais@users.noreply.github.com>
2021-11-29 18:40:28 +0000

    Add files via upload

---
+++ sys_exit.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+
+"""
+Arbitrary script to exit the python script using sys.exit(1)
+Created for Git interactive learning session
+"""
+
+import os
+
+# Print hello world
+print("Hello World")
+
+# Exit the programme:
+sys.exit(1)
+
